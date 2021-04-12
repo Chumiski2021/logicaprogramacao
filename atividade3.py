c398b2f67ffe379d2ce98e6eb1d3b5f3a8e6a4a5
Nicolle Chumiski <81190683+Chumiski2021@users.noreply.github.com>
2021-04-12 18:11:33 -0300

    Create atividade3.py

---
+++ atividade3.py
@@ -0,0 +1,39 @@
+def exercicio01():
+  num = int(input("Insira o numero: "))
+  i=0
+  while i <= num:
+    if i%5 == 0 and i%2 != 0: 
+     print(i)
+    i+=1
+
+def exercio02():
+  num = int(input ('Digite o numero de elementos da lista: '))
+  lista = []
+  i = 0 
+  while i < num:
+   lista.append (input())
+   i += 1 
+  exercicio02()
+  
+
+def exercicio03():
+ lista = ( imput )
+ num = lista[i]
+ i=0
+ while i >= num:
+    if i-5 == 0 and i+5 != 0: 
+     print(i)
+    i+=1
+exercicio03 ()
+
+
+def exercicio04 ():
+  menu: (input)
+if (input):
+ imput = 'a'
+print ('PSG')
+if (input):
+  imput = 'b' 
+  print ('BAYERN')
+if (input):
+  imput = 'd'
