a3126c4ee8cdf1f8dd3d17986fce9c8c0a488f04
Nicolle Chumiski <81190683+Chumiski2021@users.noreply.github.com>
2021-04-12 22:34:45 -0300

    Create prova.py

---
+++ prova.py
@@ -0,0 +1,34 @@
+def exercicio01 (imput, notas, soma):
+ nota1 = (imput)
+ nota2 = (imput)
+ nota3 = (imput) 
+ return input (nota1 + nota2 + nota3)
+ notas = soma
+ print ('Média do aluno é: ') 
+ return soma%notas.lengh
+ 
+def exercicio02 (i):
+ lista = []
+ total_lista= 0
+ for i in range(0,len(lista)):
+  if lista [i] <= total_lista:
+     lista != []
+ elif lista [i] >= total_lista:
+      lista = []
+
+def exercicio03 ():
+ menu: (input)
+ i = 0
+ while i = 'a':
+   if (input)
+ imput = "a"
+print ('globo')
+while i = 'b':
+   if (input)
+ imput = "b"
+print ('sbt')
+while i != 'z' or 'Z':
+  if (input)
+  input = "z" or "Z"
+print (Finalizado)
+  
